dcd47adff1a46b7b5d6a548e7f5d93173794ef53
Алексей Зелепукин <aleksey.zelepukin@gmail.com>
2018-11-06 21:20:49 +0300

    homework in process

--- lesson_04/exercise_02.py
+++ lesson_04/exercise_02.py
@@ -1,1 +1,15 @@
-# 
+# 2. Написать два алгоритма нахождения i-го по счёту простого числа.
+# Первый - использовать алгоритм решето Эратосфена.
+# Второй - без использования "решета". Проанализировать скорость и сложность алгоритмов.
+
+
+def eratosthenes(n):
+    sieve = list(range(n + 1))
+    sieve[1] = 0
+    for i in sieve:
+        if i > 1:
+            for j in range(i + i, len(sieve), i):
+                sieve[j] = 0
+    return sieve
+
+print(eratosthenes(3))
