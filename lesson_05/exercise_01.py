de90e86c94db8bc8ae5e099194aa5237f6916569
Алексей Зелепукин <aleksey.zelepukin@gmail.com>
2018-11-09 22:44:18 +0300

    homework in process

--- lesson_05/exercise_01.py
+++ lesson_05/exercise_01.py
@@ -5,3 +5,52 @@
 # и отдельно вывести наименования предприятий, чья прибыль ниже среднего.
 # Примечание: 4 квартала - это 4 разных прибыли ;-)
 
+from collections import defaultdict
+from random import randint
+
+# n = int(input('Введите количество предприятий: '))
+
+n = randint(1, 10)
+
+while n < 1:
+    print('Число предприятий должно быть положительным. Попробуйте еще раз!')
+    n = int(input('Введите количество предприятий: '))
+
+annual_statement = defaultdict(float)
+
+annual_income = 0
+total_income = 0
+
+company = None
+income = 0
+
+for i in range(n):
+    # company = input(f'Введите название предприятия #{i + 1}: ')
+    company = 'Company_' + str(i)
+    annual_income = 0
+    for j in range(4):
+        # income = float(input(f'Введите выручку за квартал #{j + 1} предприятия {company}: '))
+        income = randint(0, 100)
+        annual_income += income
+        total_income += income
+    annual_statement[company] = annual_income
+
+average_income = total_income / n
+
+above_average = []
+below_average = []
+
+if n == 1:
+    print(f'Введены данные по {n} предприятию.\n'
+          f'Невозможно определить предприятия с прибылью выше или ниже средней.')
+else:
+    for company, income in annual_statement.items():
+        if income > average_income:
+            above_average.append(company)
+        elif income < average_income:
+            below_average.append(company)
+    else:
+        print(f'Колчество предприятий: {n}')
+        print(f'Средняя прибыль: {average_income}')
+        print('Компании с прибылью выше средней: {}'.format(', '.join(above_average)))
+        print('Компании с прибылью ниже средней: {}'.format(', '.join(below_average)))
--- lesson_05/exercise_02.py
+++ lesson_05/exercise_02.py
@@ -5,3 +5,51 @@
 # Сумма чисел из примера: [‘C’, ‘F’, ‘1’].
 # Произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 
+from collections import deque, defaultdict
+
+
+def hex_sum(a, b):
+    digit_number_a = len(a)
+    digit_number_b = len(b)
+    digit_number_diff = abs(digit_number_a - digit_number_b)
+
+    deq_a = deque(a)
+    deq_b = deque(b)
+
+    if digit_number_a > digit_number_b:
+        deq_b.extendleft('0' for _ in range(digit_number_diff))
+    elif digit_number_b > digit_number_a:
+        deq_a.extendleft('0' for _ in range(digit_number_diff))
+
+    deq_a.reverse()
+    deq_b.reverse()
+
+    hex_digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+
+    dec2hex = defaultdict(str)
+    hex2dec = defaultdict(int)
+
+    for index, value in enumerate(hex_digits):
+        dec2hex[index] = value
+        hex2dec[value] = index
+
+    hex_sum = deque()
+    dec_sum = 0
+    digit = 0
+    boost = 0
+
+    for i, j in zip(deq_a, deq_b):
+        dec_sum = hex2dec[i] + hex2dec[j]
+        digit = dec_sum % 16
+        if dec_sum // 16 > 0:
+            boost = 1
+            hex_sum.appendleft(dec2hex[digit])
+        else:
+            digit += boost
+            boost = 0
+            hex_sum.appendleft(dec2hex[digit])
+
+    return list(hex_sum)
+
+
+print(hex_sum('A2', 'C4F'))
